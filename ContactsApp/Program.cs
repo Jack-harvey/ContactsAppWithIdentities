fab1c856ba04276133da9b7305b2166bcdc8ae9b
Jack Harvey <JackHarvey.00@gmail.com>
2022-04-20 15:44:49 +1000

    Change the connectionString name

--- ContactsApp/Program.cs
+++ ContactsApp/Program.cs
@@ -14,29 +14,30 @@ try
     var builder = WebApplication.CreateBuilder(args);
 
     // Add services to the container.
-    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    var connectionString = builder.Configuration.GetConnectionString("dbConnection");
     builder.Services.AddDbContext<ApplicationDbContext>(options =>
         options.UseSqlServer(connectionString));
     builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
     builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
         .AddEntityFrameworkStores<ApplicationDbContext>();
-
     builder.Services.AddDbContext<ContactsApp.Data.ContactsAppDataContext>(options =>
     {
         options.UseSqlServer(builder.Configuration.GetConnectionString("dbConnection"));
     });
-
     builder.Services.AddControllersWithViews();
+
+    //This is used to add the theme to sharedlayout before it's a webpage
+    builder.Services.AddScoped<ContactsApp.Library.UserThemeFilterService>();
+
+    // NLog: Setup NLog for Dependency injection
+    builder.Logging.ClearProviders();
+    builder.Logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+    builder.Host.UseNLog();
 
     var app = builder.Build();
 
     // Configure the HTTP request pipeline.
     if (app.Environment.IsDevelopment())
-    {
-        app.UseMigrationsEndPoint();
-    }
-    else
     {
         app.UseExceptionHandler("/Home/Error");
         // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
