e9c96f1cba5c04a3b99bb6ab31a786b92a9b8aff
Jack Harvey <JackHarvey.00@gmail.com>
2022-04-20 16:33:01 +1000

    Add the functions from previous project

--- ContactsApp/Controllers/HomeController.cs
+++ ContactsApp/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using ContactsApp.Models;
+﻿using ContactsApp.Data;
+using ContactsApp.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -7,10 +8,47 @@ namespace ContactsApp.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ContactsAppDataContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ContactsAppDataContext context)
         {
             _logger = logger;
+            _context = context;
+        }
+
+        public JsonResult UsernameAndTheme(int userId)
+        {
+            var users = _context.Users;
+            var userIdInDb = users.Where(s => s.UserId.Equals(userId));
+            return Json(userIdInDb);
+        }
+
+        public JsonResult SaveNewThemePreference(int userId, string themePreference)
+        {
+            var users = _context.Users;
+            var userToUpdate = users.Find(userId);
+
+            if (userToUpdate == null)
+                return Json(new { result = 0, message = "Congrats, it's a baby Null... 🥰" });
+
+            if (themePreference == "dracula-theme")
+            {
+                userToUpdate.ThemeSelection = "Dracula";
+                _context.Update(userToUpdate);
+                _context.SaveChanges();
+                return Json(new { result = 1, theme = "dracula-theme", message = "It's dracula baby!" });
+            }
+            if (themePreference == "atom-one-dark-theme")
+            {
+                userToUpdate.ThemeSelection = "atomOneDark";
+                _context.Update(userToUpdate);
+                _context.SaveChanges();
+                return Json(new { result = 1, theme = "atom-one-dark-theme", message = "Click change theme again, you did it wrong." });
+            }
+            else
+            {
+                return Json(new { result = 0, message = "Invalid theme" });
+            }
         }
 
         public IActionResult Index()
